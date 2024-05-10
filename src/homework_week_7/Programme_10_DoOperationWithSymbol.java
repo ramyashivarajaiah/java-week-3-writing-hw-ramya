1d421b7306eb24ebdfefc365710567d123b4e0fc
ramyashivarajaiah <rnrramya03@gmail.com>
2024-05-10 20:02:47 +0100

    homework done

---
+++ src/homework_week_7/Programme_10_DoOperationWithSymbol.java
@@ -0,0 +1,38 @@
+package homework_week_7;
+
+import java.util.Scanner;
+
+/**
+ * 10.Write a java program to input any two number and ask user to enter their symbol (+, -,
+ * /, *) find addition, Subtraction, multiplication and division according to their symbol
+ * (using if else)
+ */
+public class Programme_10_DoOperationWithSymbol {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int X = scanner.nextInt();
+        System.out.println("Enter the second number");
+        int Y = scanner.nextInt();
+        System.out.println("Please select the calculation symbol +, -, *, / : ");
+        char symbol = scanner.next().charAt(0);
+        doOperationBySelectingSymbol(X, Y, symbol);
+        //closing the scanner
+        scanner.close();
+    }
+
+    // Calculating operation based on symbol
+    public static void doOperationBySelectingSymbol(int x, int y, char symbol) {
+        if (symbol == '+') {
+            System.out.println(x + " + " + y + " = " + (x + y));
+        } else if (symbol == '-') {
+            System.out.println(x + " - " + y + " = " + (x - y));
+        } else if (symbol == '*') {
+            System.out.println(x + " * " + y + " = " + (x * y));
+        } else if (symbol == '/') {
+            System.out.println(x + " / " + y + " = " + (x / y));
+        } else {
+            System.out.println("Please enter correct symbol +,-,*,/");
+        }
+    }
+}
